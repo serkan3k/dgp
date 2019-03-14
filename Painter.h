c66e121a711e8f3071999bf6c6a4b0e0e239caff
serkan3k <serkan3k@yahoo.com>
2019-03-14 20:58:47 +0300

    init

---
+++ Painter.h
@@ -0,0 +1,20 @@
+#pragma once
+#define HAVE_SINGLEPRECISION_MATH
+#define _CRT_SECURE_NO_WARNINGS
+#define COIN_DLL
+#define HAVE_INT8_T
+#include <Inventor/nodes/SoMaterial.h>
+#include <Inventor/nodes/SoCoordinate3.h>
+#include <Inventor/nodes/SoIndexedFaceSet.h>
+#include <Inventor/nodes/SoSeparator.h>
+#include <Inventor/nodes/SoShapeHints.h>
+//#include <Inventor/nodes/SoCone.h>
+
+#include "Mesh.h"
+
+
+class Painter
+{
+public:
+	SoSeparator* getShapeSep(Mesh* mesh);
+};
