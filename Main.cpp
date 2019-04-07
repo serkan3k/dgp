e5e337a95a4e1f4f978843afae6309bc07792686
serkan3k <serkan3k@yahoo.com>
2019-04-07 21:28:59 +0300

    included Eigen library

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,7 @@
 #include <Inventor/nodes/SoSphere.h>
 #include <Inventor/nodes/SoTransform.h>
 #include <Inventor/nodes/SoMaterial.h>
+#include <Eigen/Dense>
 
 
 #include "Mesh.h"
@@ -20,9 +21,17 @@
 #include <set>
 #include <math.h>
 
+using Eigen::MatrixXd;
 
 int main(int, char ** argv)
 {
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
+
 	HWND window = SoWin::init(argv[0]);
 	SoWinExaminerViewer * viewer = new SoWinExaminerViewer(window);
 	SoSeparator * root = new SoSeparator;
